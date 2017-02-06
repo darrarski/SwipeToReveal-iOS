c5b060caa96fc9247c4bf3d88b82e8c7108a1493
Dariusz Rybicki <darrarski@gmail.com>
2017-02-06 19:23:55 +0100

    Update SwipeToRevealView
    - add rightView subview (mutable)
    - update view hierarchy and layout

--- Source/SwipeToRevealView.swift
+++ Source/SwipeToRevealView.swift
@@ -5,6 +5,8 @@ class SwipeToRevealView: UIView {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        loadSubviews()
+        setupLayout()
     }
 
     @available(*, unavailable)
@@ -12,16 +14,56 @@ class SwipeToRevealView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 
+    // MARK: Subviews
+
     var contentView: UIView? {
         didSet {
             if let oldValue = oldValue {
                 oldValue.removeFromSuperview()
             }
             if let newValue = contentView {
-                addSubview(newValue)
-                newValue.snp.makeConstraints { $0.edges.equalTo(0) }
+                centerContainer.addSubview(newValue)
+                newValue.snp.makeConstraints { $0.edges.equalToSuperview() }
             }
         }
     }
+
+    var rightView: UIView? {
+        didSet {
+            if let oldValue = oldValue {
+                oldValue.removeFromSuperview()
+            }
+            if let newValue = rightView {
+                rightContainer.addSubview(newValue)
+                newValue.snp.makeConstraints { $0.edges.equalToSuperview() }
+            }
+        }
+    }
+
+    private func loadSubviews() {
+        addSubview(centerContainer)
+        addSubview(rightContainer)
+    }
+
+    private let centerContainer = UIView(frame: .zero)
+    private let rightContainer = UIView(frame: .zero)
+
+    // MARK: Layout
+
+    private func setupLayout() {
+        centerContainer.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.bottom.equalToSuperview()
+            $0.size.equalToSuperview()
+            horizontalOffsetConstraint = $0.left.equalToSuperview().constraint
+        }
+        rightContainer.snp.makeConstraints {
+            $0.top.equalTo(centerContainer.snp.top)
+            $0.left.equalTo(centerContainer.snp.right)
+            $0.bottom.equalTo(centerContainer.snp.bottom)
+        }
+    }
+
+    private var horizontalOffsetConstraint: Constraint?
 
 }
