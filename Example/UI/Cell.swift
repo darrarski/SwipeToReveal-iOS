b34583ff457494e1b9ffd55539a33083b4c8575c
Dariusz Rybicki <darrarski@gmail.com>
2017-02-06 19:24:46 +0100

    Update Cell
    Add buttons, update layout, introduce private Factory

--- Example/UI/Cell.swift
+++ Example/UI/Cell.swift
@@ -16,41 +16,72 @@ class Cell: UITableViewCell {
 
     // MARK: Subviews
 
-    let label: UILabel = {
-        let label = UILabel(frame: .zero)
-        label.font = UIFont.systemFont(ofSize: UIFont.systemFontSize)
-        label.textColor = .darkText
-        return label
-    }()
-
+    let label = Factory.label
     let swipeToReveal = SwipeToRevealView(frame: .zero)
+    let swipeButtonA = Factory.button(title: "Button A", color: .red)
+    let swipeButtonB = Factory.button(title: "Button B", color: .blue)
 
     private func loadSubviews() {
         contentView.addSubview(swipeToReveal)
         swipeToReveal.contentView = swipeContentView
         swipeContentView.addSubview(label)
+        swipeToReveal.rightView = swipeRightView
+        swipeRightView.addSubview(swipeButtonA)
+        swipeRightView.addSubview(swipeButtonB)
     }
 
-    private let swipeContentView: UIView = {
-        let view = UIView(frame: .zero)
-        view.layoutMargins = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
-        return view
-    }()
+    private let swipeContentView = Factory.view(layoutMargins: UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8))
+    private let swipeRightView = Factory.view()
 
     // MARK: Layout
 
     private func setupLayout() {
         swipeToReveal.snp.makeConstraints {
-            $0.edges.equalTo(0)
+            $0.edges.equalToSuperview()
         }
         label.snp.makeConstraints {
             guard let superview = label.superview else { fatalError() }
-            $0.centerYWithinMargins.equalTo(0)
+            $0.centerYWithinMargins.equalToSuperview()
             $0.top.greaterThanOrEqualTo(superview.snp.topMargin)
             $0.left.equalTo(superview.snp.leftMargin)
             $0.right.lessThanOrEqualTo(superview.snp.rightMargin)
             $0.bottom.lessThanOrEqualTo(superview.snp.bottomMargin)
         }
+        swipeButtonA.snp.makeConstraints {
+            $0.top.left.bottom.equalToSuperview()
+        }
+        swipeButtonB.snp.makeConstraints {
+            $0.left.equalTo(swipeButtonA.snp.right)
+            $0.top.right.bottom.equalToSuperview()
+        }
     }
 
+}
+
+private extension Cell {
+    struct Factory {
+
+        static var label: UILabel {
+            let label = UILabel(frame: .zero)
+            label.font = UIFont.systemFont(ofSize: UIFont.systemFontSize)
+            label.textColor = .darkText
+            return label
+        }
+
+        static func button(title: String, color: UIColor) -> UIButton {
+            let button = UIButton(frame: .zero)
+            button.setTitle(title, for: .normal)
+            button.setTitleColor(.white, for: .normal)
+            button.backgroundColor = color
+            button.contentEdgeInsets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+            return button
+        }
+
+        static func view(layoutMargins: UIEdgeInsets = .zero) -> UIView {
+            let view = UIView(frame: .zero)
+            view.layoutMargins = layoutMargins
+            return view
+        }
+
+    }
 }
