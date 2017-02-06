a0adeb87c57083eed53b6315a30029b86c083459
Dariusz Rybicki <darrarski@gmail.com>
2017-02-06 18:07:47 +0100

    Add Xcode project

---
+++ Example/UI/ViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func loadView() {
+        super.loadView()
+        view.backgroundColor = .white
+    }
+
+}
