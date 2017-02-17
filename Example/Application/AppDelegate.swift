669e1c90c7e9770195fb59e9dcfb9d56efd3c7df
Dariusz Rybicki <darrarski@gmail.com>
2017-02-18 00:52:43 +0100

    Update AppDelegate to use assembly

--- Example/Application/AppDelegate.swift
+++ Example/Application/AppDelegate.swift
@@ -1,14 +1,27 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder {
 
+    override convenience init() {
+        self.init(assembly: Container().appDelegateAssembly)
+    }
+
+    init(assembly: AppDelegateAssembly) {
+        self.assembly = assembly
+        super.init()
+    }
+
+    fileprivate let assembly: AppDelegateAssembly
     var window: UIWindow?
+
+}
+
+extension AppDelegate: UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = TableViewController()
+        window = assembly.window
         window?.makeKeyAndVisible()
         return true
     }
