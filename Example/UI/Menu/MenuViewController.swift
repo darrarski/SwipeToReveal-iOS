1aebecb25781b50b00d4df3d06bd740613dee22f
Dariusz Rybicki <darrarski@gmail.com>
2017-02-19 23:16:40 +0100

    Update MenuViewController
    - inherit from UITableViewController
    - use MenuViewModel
    - display cells for menu items

--- Example/UI/Menu/MenuViewController.swift
+++ Example/UI/Menu/MenuViewController.swift
@@ -1,9 +1,10 @@
 import UIKit
 
-class MenuViewController: UIViewController {
+class MenuViewController: UITableViewController {
 
     init(assembly: MenuAssembly) {
         self.assembly = assembly
+        self.viewModel = assembly.viewModel
         super.init(nibName: nil, bundle: nil)
     }
 
@@ -14,13 +15,32 @@ class MenuViewController: UIViewController {
 
     // MARK: View
 
-    override func loadView() {
-        super.loadView()
-        view.backgroundColor = .white
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = viewModel.title
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.tableFooterView = UIView(frame: .zero)
+    }
+
+    // MARK: UITableViewDataSource
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard section == 0 else { return 0 }
+        return viewModel.items.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard indexPath.section == 0 else { fatalError() }
+        let viewModel = self.viewModel.items[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.accessoryType = .disclosureIndicator
+        cell.textLabel?.text = viewModel.title
+        return cell
     }
 
     // MARK: Private
 
     private let assembly: MenuAssembly
+    private let viewModel: MenuViewModel
 
 }
--- ExampleTests/UI/MenuViewControllerSpec.swift
+++ ExampleTests/UI/MenuViewControllerSpec.swift
@@ -8,9 +8,11 @@ class MenuViewControllerSpec: QuickSpec {
         describe("MenuViewController") {
             var sut: MenuViewController!
             var assembly: Assembly!
+            var viewModel: ViewModel!
 
             beforeEach {
-                assembly = Assembly()
+                viewModel = ViewModel()
+                assembly = Assembly(viewModel: viewModel)
                 sut = MenuViewController(assembly: assembly)
             }
 
@@ -19,11 +21,60 @@ class MenuViewControllerSpec: QuickSpec {
                     _ = sut.view
                 }
 
-                // TODO:
+                it("should have correct title") {
+                    expect(sut.navigationItem.title).to(equal("Menu Title"))
+                }
+
+                describe("table view") {
+                    it("should have 1 section") {
+                        expect(sut.numberOfSections(in: sut.tableView)).to(equal(1))
+                    }
+
+                    it("should have 3 rows") {
+                        expect(sut.tableView(sut.tableView, numberOfRowsInSection: 0)).to(equal(3))
+                    }
+
+                    func testCell(_ row: Int) {
+                        describe("cell \(row + 1)") {
+                            var cell: UITableViewCell!
+
+                            beforeEach {
+                                cell = sut.tableView(sut.tableView, cellForRowAt: IndexPath(row: row, section: 0))
+                            }
+
+                            it("should have correct title") {
+                                expect(cell.textLabel?.text).to(equal(viewModel.items[row].title))
+                            }
+
+                            it("should have correct accessory type") {
+                                expect(cell.accessoryType).to(equal(UITableViewCellAccessoryType.disclosureIndicator))
+                            }
+                        }
+                    }
+
+                    testCell(0)
+                    testCell(1)
+                    testCell(2)
+                }
             }
         }
     }
 
-    struct Assembly: MenuAssembly {}
+    struct Assembly: MenuAssembly {
+        let viewModel: MenuViewModel
+    }
+
+    struct ViewModel: MenuViewModel {
+        let title = "Menu Title"
+        let items: [MenuItemViewModel] = [
+            ItemViewModel(title: "Menu Item 1"),
+            ItemViewModel(title: "Menu Item 2"),
+            ItemViewModel(title: "Menu Item 3")
+        ]
+    }
+
+    struct ItemViewModel: MenuItemViewModel {
+        let title: String
+    }
 
 }
