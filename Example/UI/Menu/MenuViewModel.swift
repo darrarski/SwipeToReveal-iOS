91cd8dcd9039eb5369ca35bdfdc35e46b56ac2fc
Dariusz Rybicki <darrarski@gmail.com>
2017-02-20 00:52:58 +0100

    Add MenuViewModelDelegate protocol

--- Example/UI/Menu/MenuViewModel.swift
+++ Example/UI/Menu/MenuViewModel.swift
@@ -1,4 +1,11 @@
-protocol MenuViewModel {
+import UIKit
+
+protocol MenuViewModel: class {
+    weak var delegate: MenuViewModelDelegate? { get set }
     var title: String { get }
     var items: [MenuItemViewModel] { get }
+}
+
+protocol MenuViewModelDelegate: class {
+    func menuViewModel(_ viewModel: MenuViewModel, presentViewController viewController: UIViewController)
 }
