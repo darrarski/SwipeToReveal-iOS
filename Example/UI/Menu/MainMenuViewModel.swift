67533fab917a09d188dc6dbddec845d3cdb29ec4
Dariusz Rybicki <darrarski@gmail.com>
2017-02-20 00:53:58 +0100

    Update MainMenuViewModel

--- Example/UI/Menu/MainMenuViewModel.swift
+++ Example/UI/Menu/MainMenuViewModel.swift
@@ -1,9 +1,33 @@
-class MainMenuViewModel: MenuViewModel {
+import UIKit
+
+class MainMenuViewModel: MenuViewModel, MenuItemViewModelDelegate {
+
+    init(assembly: MenuAssembly) {
+        self.assembly = assembly
+        tableViewExample.delegate = self
+    }
+
+    weak var delegate: MenuViewModelDelegate?
+    private let assembly: MenuAssembly
+    private let tableViewExample = MainMenuItemViewModel(title: "Table View Cell Example")
+
+    // MARK: MenuViewModel
 
     let title = "SwipeToReveal"
 
-    let items: [MenuItemViewModel] = [
-        PushMenuItemViewModel(title: "Table View Cell Example")
-    ]
+    var items: [MenuItemViewModel] {
+        return [tableViewExample]
+    }
+
+    // MARK: MenuItemViewModelDelegate
+
+    func menuItemViewModelDidSelect(_ viewModel: MenuItemViewModel) {
+        if viewModel === tableViewExample {
+            let viewController = assembly.tableViewExampleController
+            delegate?.menuViewModel(self, presentViewController: viewController)
+        } else {
+            fatalError()
+        }
+    }
 
 }
