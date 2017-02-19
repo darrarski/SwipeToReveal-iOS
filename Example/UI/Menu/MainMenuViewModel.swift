90596eb3316801d3f5f281d1b480fc9b52837b3a
Dariusz Rybicki <darrarski@gmail.com>
2017-02-19 23:41:44 +0100

    Update MainMenuViewModel

--- Example/UI/Menu/MainMenuViewModel.swift
+++ Example/UI/Menu/MainMenuViewModel.swift
@@ -1,11 +1,9 @@
 class MainMenuViewModel: MenuViewModel {
 
     let title = "SwipeToReveal"
-    
+
     let items: [MenuItemViewModel] = [
-        PushMenuItemViewModel(title: "Menu Item 1"),
-        PushMenuItemViewModel(title: "Menu Item 2"),
-        PushMenuItemViewModel(title: "Menu Item 3")
+        PushMenuItemViewModel(title: "Table View Cell Example")
     ]
 
 }
