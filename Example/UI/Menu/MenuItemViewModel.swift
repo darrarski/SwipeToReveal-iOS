939082420acae9a8dc03a6ca6d071ac7865b7698
Dariusz Rybicki <darrarski@gmail.com>
2017-02-20 00:52:34 +0100

    Add MenuItemViewModelDelegate protocol

--- Example/UI/Menu/MenuItemViewModel.swift
+++ Example/UI/Menu/MenuItemViewModel.swift
@@ -1,4 +1,9 @@
-protocol MenuItemViewModel {
+protocol MenuItemViewModel: class {
+    weak var delegate: MenuItemViewModelDelegate? { get set }
     var title: String { get }
     func select()
+}
+
+protocol MenuItemViewModelDelegate: class {
+    func menuItemViewModelDidSelect(_ viewModel: MenuItemViewModel)
 }
