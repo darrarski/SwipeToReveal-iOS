468b950caf8c7970ef1fc6e34dfd215228c7b2d2
Dariusz Rybicki <darrarski@gmail.com>
2017-02-19 23:14:22 +0100

    Update Container_MenuAssembly extension

--- Example/UI/Menu/Container_MenuAssembly.swift
+++ Example/UI/Menu/Container_MenuAssembly.swift
@@ -2,7 +2,13 @@ import UIKit
 
 extension Container {
     var menuAssembly: MenuAssembly {
-        struct Assembly: MenuAssembly {}
+        struct Assembly: MenuAssembly {
+
+            var viewModel: MenuViewModel {
+                return MainMenuViewModel()
+            }
+
+        }
         return Assembly()
     }
 }
